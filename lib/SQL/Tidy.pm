bfde1f9a7a47a1df7fdbee34c694373776fcc30c
Gregory Siems <gsiems@gmail.com>
2017-08-14 14:27:45 -0500

    Add "new" and stub for "tidy" functions.

--- lib/SQL/Tidy.pm
+++ lib/SQL/Tidy.pm
@@ -5,6 +5,8 @@ use warnings;
 use Carp();
 use Data::Dumper;
 
+use SQL::Tidy::Tokenize;
+
 =head1 NAME
 
 SQL::Tidy
@@ -25,7 +27,48 @@ Create, and return, a new instance of this
 
 =cut
 
-#
+sub new {
+    my ( $this, $args ) = @_;
+    my $class = ref($this) || $this;
+    my $self  = {};
+    bless $self, $class;
+
+    # Set defaults if not otherwise set
+    $args->{use_tabs}       ||= 0; # Spaces all the way
+    $args->{tab_size}       ||= 4;
+    $args->{max_line_width} ||= 120;
+    $args->{min_line_width} ||= 40;
+    $args->{keywords_case}  ||= 'upper';
+    $args->{case_folding}   ||= 'upper'; # It is in the SQL standard after all
+
+    # Regexp for "Safe-to-unquote" identifiers
+    # Not that this will probably ever make sense as a config option...
+    $args->{stu_ident}      ||= '[A-Z0-9_]+';
+    my stu_ident = $args->{stu_ident};
+    $args->{stu_re}         ||= qr/^"$stu_ident"$/;
+
+    foreach my $key ( keys %{$args} ) {
+        unless ( exists $args->{$key} ) {
+            $self->{$_} = $args->{$_};
+        }
+    }
+
+    return $self;
+}
+
+=item tidy ( code )
+
+Takes a string of code, formats it, and returns the result
+
+=cut
+
+sub tidy {
+    my ( $self, $code ) = @_;
+
+    # TODO: This is a stub...
+
+    return $code;
+}
 
 =back
 
