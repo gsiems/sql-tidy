5737a06c8d6a00bbf7a120475371ef8a31dcfcd6
Gregory Siems <gsiems@gmail.com>
2017-08-19 09:41:51 -0500

    Work on formatting. Added hooks for capitalizing keywords and unquoting identifiers.

--- lib/SQL/Tidy/DDL.pm
+++ lib/SQL/Tidy/DDL.pm
@@ -16,6 +16,7 @@ SQL::Tidy::DDL
 
 =cut
 
+my $Dialect;
 my $indenter;
 
 =item new
@@ -30,6 +31,7 @@ sub new {
     my $self = {};
     bless $self, $class;
 
+    $Dialect  = SQL::Tidy::Dialect->new($args);
     $indenter = SQL::Tidy::Indent->new($args);
 
     return $self;
@@ -37,8 +39,29 @@ sub new {
 
 sub format_ddl {
     my ( $self, $comments, @tokens ) = @_;
-    my @new_tokens = $self->add_vspace( $comments, @tokens );
+    my @new_tokens = @tokens;
+
+    @new_tokens = $self->unquote_identifiers(@new_tokens);
+    @new_tokens = $self->capitalize_keywords(@new_tokens);
+
+    @new_tokens = $self->add_vspace( $comments, @tokens );
     @new_tokens = $self->add_indents(@new_tokens);
+
+    return @new_tokens;
+}
+
+sub capitalize_keywords {
+    my ( $self, @tokens ) = @_;
+    return @tokens;    # TODO remove me
+    my @new_tokens;
+
+    return @new_tokens;
+}
+
+sub unquote_identifiers {
+    my ( $self, @tokens ) = @_;
+    return @tokens;    # TODO remove me
+    my @new_tokens;
 
     return @new_tokens;
 }
@@ -46,17 +69,94 @@ sub format_ddl {
 sub add_vspace {
     my ( $self, $comments, @tokens ) = @_;
     my @new_tokens;
+    my $parens      = 0;
+    my $in_proc_sig = 0;
+    my $in_view_sig = 0;
+    my $current     = '';
+
+    my %h = (
+        'CREATE'  => 1,
+        'ALTER'   => 1,
+        'DROP'    => 1,
+        'COMMENT' => 1,
+        'GRANT'   => 1,
+        'REVOKE'  => 1,
+    );
 
     foreach my $idx ( 0 .. $#tokens ) {
         my $token       = uc $tokens[$idx];
         my $line_before = 0;
         my $line_after  = 0;
 
-        if ( $token eq ';' ) {
+        if ( not $current and exists $h{$token} ) {
+            $current = $token;
+        }
+
+        # Force function/procedure signatures to line-wrap
+        if ( $current eq 'CREATE' ) {
+            if ( $token eq 'FUNCTION' or $token eq 'PROCEDURE' ) {
+                $in_proc_sig = 1;
+            }
+            elsif ( $token eq 'VIEW' ) {
+                $in_view_sig = 1;
+            }
+        }
+
+        if ( $token eq '(' ) {
+            $parens++;
+            if ( $in_proc_sig and $parens == 1 ) {
+                $line_after = 1;
+            }
+            elsif ( $in_view_sig and $parens == 1 ) {
+                $line_after = 1;
+            }
+        }
+        elsif ( $token eq ')' ) {
+            $parens--;
+            if ( $in_proc_sig and $parens == 0 ) {
+                $line_after = 1;
+            }
+            elsif ( $in_view_sig and $parens == 0 ) {
+                $line_after = 1;
+            }
+        }
+        elsif ( $token eq ',' ) {
+            if ( $in_proc_sig and $parens == 1 ) {
+                $line_after = 1;
+            }
+            elsif ( $in_view_sig and $parens == 1 ) {
+                $line_after = 1;
+            }
+        }
+
+        elsif ( $token eq ';' ) {
+            $current    = 0;
             $line_after = 1;
         }
         elsif ( $token eq '/' ) {
             $line_after = 1;
+        }
+        elsif ( $token eq 'AS' ) {
+            if ( $in_proc_sig and $parens == 0 ) {
+                $in_proc_sig = 0;
+                $line_before = 1;
+                $line_after  = 1;
+            }
+            elsif ( $in_view_sig and $parens == 0 ) {
+                $in_view_sig = 0;
+                $line_before = 1;
+                $line_after  = 1;
+            }
+        }
+        elsif ( $token eq 'IS' ) {
+            if ( $in_proc_sig and $parens == 0 ) {
+                $in_proc_sig = 0;
+                $line_before = 1;
+                $line_after  = 1;
+            }
+        }
+        elsif ( $token eq 'RETURN' ) {
+            $line_before = 1;
         }
         elsif ( $token =~ m/^~~comment_/i ) {
             $line_before = $comments->{ lc $token }{newline_before};
@@ -89,11 +189,37 @@ sub add_vspace {
 sub add_indents {
     my ( $self, @tokens ) = @_;
     my @new_tokens;
+    my $parens = 0;
 
     foreach my $idx ( 0 .. $#tokens ) {
-        my $token = uc $tokens[$idx];
+        my $token      = uc $tokens[$idx];
+        my $next_token = ( $idx < $#tokens ) ? uc $tokens[ $idx + 1 ] : '';
+        my $last_token = ( $idx > 0 ) ? uc $tokens[ $idx - 1 ] : '';
 
-        push @new_tokens, $tokens[$idx];
+        if ( $token eq '(' ) {
+            $parens++;
+        }
+        elsif ( $token eq ')' ) {
+            $parens--;
+        }
+
+        if ( $token eq "\n" ) {
+
+            push @new_tokens, "\n";
+
+            if ( $next_token ne "\n" ) {
+                my $indent = $indenter->to_indent($parens);
+                #push @new_tokens, "-- " . join (', ', scalar @block_stack, $parens, $offset );
+                if ($indent) {
+                    push @new_tokens, $indent;
+                }
+            }
+
+        }
+        else {
+
+            push @new_tokens, $tokens[$idx];
+        }
 
     }
 
